1451d288c499880e2403eb8da9db9b17ba226590
vishwajeetsuhaspatil <bhagyashreesushv2@gmail.com>
2024-09-14 20:45:56 +0530

    operation in dictionary

---
+++ operation.py
@@ -0,0 +1,12 @@
+a= {"name": "vishwajeet","Age": 14, "Gender": "Male"}
+print(a["name"])
+a["age"]= 10000
+print(a)
+p= {"codingal":2,"is":2,"for":2,"coding":1}
+print(str(p))
+k= 2
+result= 0
+for i in p:
+    if p[i]==k:
+        result= result+1
+print(str(result))        
